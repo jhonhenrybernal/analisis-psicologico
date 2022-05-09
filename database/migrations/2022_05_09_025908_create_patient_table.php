2937503591f26b0e7ce0c5d02dcedbbf149e99b3
Jhon <jhonhenrybernal@gmail.com>
2022-05-08 22:15:47 -0500

    integracion nuevas tablas para  registro paciente

---
+++ database/migrations/2022_05_09_025908_create_patient_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('forstName');
+            $table->string('lastName');
+            $table->string('phoneWhatssap');
+            $table->string('email');
+            $table->string('status_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patient');
+    }
+}
---
+++ database/migrations/2022_05_09_030148_create_assessment_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssessmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assessments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('patients_id');
+            $table->integer('code_invitation');
+            $table->integer('status_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assessment');
+    }
+}
